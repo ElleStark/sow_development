6b94d788c450ad901570e8ccb0fec499ef637f6f
ElleStark <elle.stark5645@gmail.com>
2023-06-02 14:51:41 -0600

    Tested & plotted other distance metrics

--- scripts/initial_sow_n_MSTmean.R
+++ scripts/initial_sow_n_MSTmean.R
@@ -7,6 +7,7 @@ library(DiceDesign)
 library(ggplot2)
 library(tidyverse)
 library(clhs)
+library(proxy)
 
 
 ############## OBTAIN DATA #############################
@@ -17,28 +18,41 @@ full_factorial_sow_norm <- read.csv('data/ignore/full_factorial_sow_norm.csv', h
 
 # cLHS algorithm selected based on analysis in initial_sow_sampling script
 iter <-  10000
-set.seed(26)
 
 # Set max SOW to 1,000 to be able to complete a robustness run in a reasonable time
-nsow_list <-  c(100, 300, 500, 750, 1000, 50, 2000, 200, 1500)
+nsow_list <-  c(50, 100, 200, 300, 500, 750, 1000, 1500, 2000)
 mstmean_list <- c()
 mstsd_list <- c()
+avgdist_list <- c()
+mindist_list <- c()
 
 for (i in 1:length(nsow_list)){
   nsow <- nsow_list[i]
   
-  i=9
-  nsow=1500
+  set.seed(26)
   clhs_samp <- clhs(full_factorial_sow_norm, size=nsow, iter=iter, 
        simple = F, weights = list(numeric=1, factor=1, correlation=1))$sampled_data
+  
+  dist_mat <- dist(clhs_samp, method = 'euclidean')
+  avgdist_list[i] <- mean(dist_mat)
+  mindist_list[i] <- min(dist_mat)
   
   mststats <- mstCriteria(clhs_samp)$stats
   mstmean_list[i] <- mststats[1]
   mstsd_list[i] <- mststats[2]
 }
 
-mst_df <- data.frame('n' = nsow_list, 'MSTmean' = mstmean_list)
+dist_df <- data.frame('n' = nsow_list, 'MSTmean' = mstmean_list, 
+                      'MSTsd'= mstsd_list, 'MinDist' = mindist_list, 'AvgDist' = avgdist_list)
 
-rough_mst_plot <- ggplot(data = mst_df) +
-  geom_point(mapping = aes(x=n, y=MSTmean)) +
+dist_df_long <- pivot_longer(dist_df, cols = c(2:5), names_to = 'Metric', values_to = 'Value')
+
+rough_dist_plot <- ggplot(data = dist_df_long, aes(x=n, y=Value, col = Metric)) +
+  geom_point() +
+  theme_bw()
+
+mstdist_df <- filter(dist_df_long, Metric=='MinDist')
+
+mindist_plot <- ggplot(data = mstdist_df, aes(x=n, y=Value)) +
+  geom_point() +
   theme_bw()
