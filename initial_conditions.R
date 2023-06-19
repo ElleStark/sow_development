6406e7278b357f61ded2d27369f94befb0ea6e6d
ElleStark <elle.stark5645@gmail.com>
2023-06-19 06:32:56 -0600

    Final SCD sampling with dist calcs

--- initial_conditions.R
+++ initial_conditions.R
@@ -12,6 +12,14 @@ library(shiny)
 library(CRSSIO)
 library(DiceDesign)
 library(GGally)
+library(clhs)
+
+############## FUNCTIONS #################
+
+# function to normalize a single column (or vector) so that values are between 0 and 1
+normalize_col <- function(x){
+  (x - min(x))/(max(x)-min(x))
+}
 
 dist_over_hist <- function(data){
   # Input: vector of data that you want to fit 
@@ -185,7 +193,7 @@ pobs_mat <- as.matrix(cbind(mead_pobs, powell_pobs))
 
 # You can run Shiny app to explore and compare different copulas - Vinecopula package
 # Can also select copula here, especially if you don't want the top fit determined by BiCopSelect
-selected_cop <- BiCopCompare(mead_pobs, powell_pobs, familyset = NA, rotations = TRUE)
+#selected_cop <- BiCopCompare(mead_pobs, powell_pobs, familyset = NA, rotations = TRUE)
 
 # Select Cop model
 selected_cop <- BiCopSelect(mead_pobs, powell_pobs,familyset=NA)
@@ -223,31 +231,8 @@ scatterplot3d(v[,1],v[,2], cdf_mvd, color="red", main="CDF", xlab = "Mead (ft)",
 # persp(joint_dist, pMvdc, xlim = c(m_low, m_high), ylim=c(p_low, p_high), main = "CDF")
 # contour(joint_dist, pMvdc, xlim = c(m_low, m_high), ylim=c(p_low, p_high), main = "Contour plot")
 
-
-### Check random seed effect for random sampling:
-for (i in 1:30){
-  set.seed(i)
-  initial_conditions <- NA
-  initial_conditions <- as.data.frame(rMvdc(1000, joint_dist)) %>%
-    rename('mead' = 'V1', 'powell' = 'V2')
-  
-  plot_compare <- NA
-  plot_compare <- ggplot() +
-    geom_point(data = initial_conditions, mapping = aes(x=mead, y=powell), color = 'red') +
-    geom_point(data = long, mapping = aes(x=mead_pe, y=powell_pe), color='blue') +
-    xlab('Mead Elevation (ft)') +
-    ylab('Powell Elevation (ft)') +
-    ylim(3400, 3750) +
-    xlim(900, 1150) +
-    theme_bw()
-  
-  #print(plot_compare)
-  ggsave(filename = paste0('data/temp/unifsamps_', i, '.png'), plot_compare, width=4, height=4)
-}
-# Random seed 14 appears to give more even distribution
-
 # Determine number of samples and generate initial conditions dataframe
-set.seed(14)
+set.seed(30) # Seed 30 selected because yielded highest MSTmean testing1 to 100
 initial_conditions <- as.data.frame(rMvdc(1000, joint_dist)) %>%
   rename('mead' = 'V1', 'powell' = 'V2')
 
@@ -290,13 +275,13 @@ get_lhsd_samp <- function(random_samples){
 }
 
 # Get initial conditions previously generated from copula analysis
-initial_conditions <- readRDS('data/outputs/init_cond_1000.rds')
+#initial_conditions <- readRDS('data/outputs/init_cond_1000.rds')
 
 # Generate demand from uniform dist. Demand min/max per 2020 Robustness runs.
 n <-1000
 min_demand <- 4.2
 max_demand <- 6.0 
-set.seed(4) # Random seed 4 appears to give good distribution for demand
+#set.seed(4) # Random seed 4 appears to give good distribution for demand
 
 demand <-  runif(n = n, min = min_demand, max = max_demand)
 
@@ -305,15 +290,15 @@ scd_df$demand <- demand
 scd_df$method <- 'Random'
 
 # Plot of original randomly sampled SCD points
-orig_pairwise <- ggpairs(scd_df)
+orig_pairwise <- ggpairs(scd_df[,1:3])
 
 # Generate LHSD design (probability of each dimension for each point)
-scd_lhsd_probs <- get_lhsd_samp(scd_df)
+scd_lhsd_probs <- get_lhsd_samp(scd_df[,1:3])
 
 # Map back onto marginal distributions to get values for each dimension
 # NEED TO MANUALLY CHANGE BELOW CODE TO MATCH DISTRIBUTIONS FITTED TO MARGINS
 mead_samps <- qweibull(scd_lhsd_probs[,1], shape = 41.9, scale = 1074)
-powell_samps <- qnorm(scd_lhsd_probs[,2], mean = 3596.1, sd = 44.3)
+powell_samps <- qnorm(scd_lhsd_probs[,2], mean = 3596, sd = 44.3)
 demand_samps <- qunif(scd_lhsd_probs[,3], min = 4.2, max = 6.0)
 scd_lhsd <- data.frame('mead' = mead_samps, 'powell' = powell_samps, 
                        'demand' = demand_samps, 'method' = 'LHSD')
@@ -324,7 +309,7 @@ scd_lhsd <- scd_lhsd %>%
   mutate(across(mead, ~ ifelse(. > 1229, 1229, .))) %>%
   mutate(across(mead, ~ ifelse(. < 895, 895, .)))
 
-lhsd_pairwise <- ggpairs(as.data.frame(scd_lhsd), columns = c('mead', 'powell', 'demand'))
+lhsd_pairwise <- ggpairs(as.data.frame(scd_lhsd_probs), columns = c('mead', 'powell', 'demand'))
 
 ### Compare random sampling to LHSD - pairwise plots
 scd_combined <- rbind(scd_lhsd, scd_df)
@@ -347,12 +332,25 @@ plot_compare <- ggplot() +
 plot_compare
 
 ### Compare cLHS to LHSD - pairwise plots
-clhs_scd <- readRDS(scd_df, file = 'data/temp/scd_df.rds')
+full_factorial_scd <- uncount(initial_conditions, length(demand))
+full_factorial_scd <- full_factorial_scd %>%
+  mutate(demand = rep(demand, nrow(initial_conditions)))
+
+full_factorial_scd_norm <- full_factorial_scd %>%
+  mutate(across(everything(), normalize_col))
+
+nscd <- 1000
+iter = 10000
+
+scd_samples <- clhs(full_factorial_scd_norm, size=nscd, iter=iter, 
+                    simple = F, weights = list(numeric=1, factor=1, correlation=1))
+
+clhs_scd <- full_factorial_scd[scd_samples$index_samples,]
 clhs_scd <- mutate(clhs_scd, method='cLHS')
 
-lhsd_v_clhs <- rbind(scd_lhsd, clhs_scd)
+lhsd_clhs <- rbind(scd_lhsd, clhs_scd)
 
-methods_pairs <- ggpairs(lhsd_v_clhs, columns = c('mead', 'powell', 'demand'), 
+methods_pairs <- ggpairs(lhsd_clhs, columns = c('mead', 'powell', 'demand'), 
                          aes(color=method, alpha = 0.5))
 
 mindist_cLHS <- mindist(clhs_scd[,1:3])
@@ -362,21 +360,3 @@ mindist_Random <- mindist(scd_df[,1:3])
 mstmean_cLHS <- mstCriteria(clhs_scd[,1:3])$stats[1]
 mstmean_LHSD <- mstCriteria(scd_lhsd[,1:3])$stats[1]
 mstmean_Random <- mstCriteria(scd_df[,1:3])$stats[1]
-
-### Possibly test imposing correlation AFTER LHS sampling
-# using Iman-Conover transformation
-
-# initial LHS design: 1000 samples. 
-# initial conditions treated as 1D, since we have a CDF for the joint distribution of Mead and Powell
-lhs_design <- lhsDesign(1000, 2, seed = 4)$design
-# optimized LHS design using maximin criteria
-maxmin_lhs <- maximinSA_LHS(lhs_design)
-
-
-# pull initial conditions samples according to optimized LHS design 
-ic_probs <- maxmin_lhs$design[,1]
-
-
-
-
-
